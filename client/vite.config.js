b17bc5172ad90fd50ad60de0150fc3de46a6b982
Julie Cheng <jucheng925@gmail.com>
2024-09-19 21:53:32 -0400

    tested out and got google places api from working, able to fetch some data points

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(env.GOOGLE_MAPS_API_KEY)
-    },
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const axios = require("axios");
+require('dotenv').config()
+
 const app = express();
 const cors = require("cors");
 const corsOptions = {
