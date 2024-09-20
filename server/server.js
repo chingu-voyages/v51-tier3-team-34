5fcdf901f47b57e7bb0c6266885108e70fca56d9
Julie Cheng <jucheng925@gmail.com>
2024-09-20 13:00:38 -0400

    formated landmark data in notepad file added to data folder located in server folder

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const axios = require("axios");
-require('dotenv').config()
+// const axios = require("axios");
+// require('dotenv').config()
 
 const app = express();
 const cors = require("cors");
@@ -16,16 +16,17 @@ app.get("/api", (req, res) => {
     res.json({"fruits": ["apple", "orange", "banana"]})
 });
 
-app.get('/historical-landmarks', async (req, res) => {
-    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-    const location = '38.0406,-84.5037'; // Latitude and Longitude for Lexington, KY
-    const radius = 10000; // Radius in meters (10 km)
+// Below used for fetching information from google places api
+// app.get('/historical-landmarks', async (req, res) => {
+//     const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+//     const location = '38.0406,-84.5037'; // Latitude and Longitude for Lexington, KY
+//     const radius = 10000; // Radius in meters (10 km)
     
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=point_of_interest&keyword=historic&key=${apiKey}`);
-    res.json(response.data.results);
+//     const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=point_of_interest&keyword=historic&key=${apiKey}`);
+//     res.json(response.data.results);
 
 
-});
+// });
 
 
 app.listen(8080, ()=> {
