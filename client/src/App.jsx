4f01f0a929785574a06454e4c6dd7fb5a4f8ddbe
Julie Cheng <jucheng925@gmail.com>
2024-09-21 09:55:57 -0400

    add fetch statement to frontend, so frontend can fetch landmarks from backend, revise read-me on how to start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,88 +9,15 @@ import {
 } from "@vis.gl/react-google-maps";
 
 
-// LAT and LNG place markers where they should be. Type and imgURL are for implementing custom images or icons.  
-const pointsOfInterest = [
-  {
-    key: "maryToddHouse",
-    location: { lat: 38.05462, lng: -84.49666 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "ashland",
-    location: { lat: 38.030517, lng: -84.484004 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "huntMorganHouse",
-    location: { lat: 38.04965, lng: -84.49588 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "lexingtonCemetery",
-    location: { lat: 38.06149, lng: -84.49049 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "waveland",
-    location: { lat: 37.95511, lng: -84.54357 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "historicMorrisonHall",
-    location: { lat: 38.039401, lng: -84.501246 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "oldFayetteCountyCourthouse",
-    location: { lat: 38.04798, lng: -84.49688 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "gratzPark",
-    location: { lat: 38.05145, lng: -84.4963 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "cheapsidePark",
-    location: { lat: 38.04854, lng: -84.49685 },
-    type: "default",
-    imgURL: "",
-  },
-  {
-    key: "trianglePark",
-    location: { lat: 38.0492, lng: -84.4994 },
-    type: "default",
-    imgURL: "",
-  },
-];
-
-
 function App() {
-
-  //TESTING - can be delete if needed
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api");
-    setArray(response.data.fruits)
-    console.log(response.data.fruits);
-  };
-
-  // const fetchLocationAPI = async () => {
-  //   const response = await axios.get("http://localhost:8080/historical-landmarks");
-  //   console.log(response.data);
-  // };
+  const [pointsOfInterest, setPointsOfInterest] = useState([])
 
   useEffect(()=> {
-    fetchAPI();
-  }, [])
+    fetch("/api/landmarks")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  })
+
 
   return (
     <>
