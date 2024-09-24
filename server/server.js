d5b92e4d2558206f7a4806326815d787ede017bf
Julie Cheng <jucheng925@gmail.com>
2024-09-23 23:52:32 -0400

    fix landmark markers

--- server/server.js
+++ server/server.js
@@ -1,45 +1,55 @@
 const express = require("express")
 const { connectToDb, getDb } = require('./db')
+const landmarks = require('./data/landmarks')
 
 
 const app = express();
 app.use(express.json())
 
 const cors = require("cors");
+
 const corsOptions = {
   origin: ["http://localhost:5173"],
 }; 
 
-app.use(cors(corsOptions));
+//remove corsOptions in development
+app.use(cors());
+//use in deployment (development branch)
+//app.use(cors(corsOptions))
 
 
 //db connection - if successful, listen for any request
 let db
 
-connectToDb((err)=> {
-  if (!err) {
-    app.listen(8080, ()=> {
-      console.log("Server started on port 8080");
-    });
-		db = getDb()
-  }
-})
+const port = process.env.PORT || 8080
 
+// connectToDb((err)=> {
+//   if (!err) {
+//     app.listen(port, ()=> {
+//       console.log(`Server started on port ${port}`);
+//     });
+// 		db = getDb()
+//   }
+// })
+app.listen(port, ()=> {
+	console.log(`Server started on port ${port}`);
+})
 
 // routes
-app.get('/api/landmarks', (req, res) => {
-	let landmarks = []
 
-	db.collection('landmarks')
-		.find()
-		.forEach(place => landmarks.push(place))
-		.then(() => {
-			res.status(200).json(landmarks)
-		})
-		.catch(()=> {
-			res.status(500).json({error: "Could not fetch"})
-		})
-})
+// app.get('/api/landmarks', (req, res) => {
+// 	let landmarks = []
+
+// 	db.collection('landmarks')
+// 		.find()
+// 		.forEach(place => landmarks.push(place))
+// 		.then(() => {
+// 			res.status(200).json(landmarks)
+// 		})
+// 		.catch(()=> {
+// 			res.status(500).json({error: "Could not fetch"})
+// 		})
+// })
 
 app.post('/api/landmarks', (req, res) => {
 	const landmark = req.body
