5b37bdb88ea0cdfc63bd986145401f5a46085a8d
Julie Cheng <jucheng925@gmail.com>
2024-10-03 16:59:20 -0400

    WIP - marker not showing up

---
+++ client/src/components/CustomMarker.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { Marker } from '@react-google-maps/api';
+
+
+const CustomMarker = () => {
+    const position = { lat: 38.052327640, lng: -84.497 }; // Valid coordinates
+
+    console.log('Rendering CustomMarker:', position); // Check if position is logged
+  
+
+  return (
+    <Marker position={position} />
+  );
+};
+
+export default CustomMarker;
--- client/src/components/ScavengerHunt.jsx
+++ client/src/components/ScavengerHunt.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
+
 import MapContainer from "./MapContainer";
+import ScavengerProcess from "./ScavengerProcess"
+import { Circle, Marker } from "@react-google-maps/api";
+import CustomMarker from "./CustomMarker";
+import PoiMarkers from "./PoiMarkers";
 
 
 
@@ -26,9 +31,20 @@ const ScavengerHunt = () => {
 
   useGeolocation(setUserLocation);
 
+
   return (
     <>
-      <MapContainer />
+      <div className="information">
+        <h2>Scavenger Hunt</h2>
+        <p>Ready to test your knowledge of Lexington, KY? Earn points playing our scavenger hunt.</p>
+        <p>If you are ready, head to downtown Lexington to the marker on the map. Once you are there, click on the button to start.</p>
+      </div>
+      <button>I am here!</button> {/* Once clicked, can turn on GPS*/ }
+      <ScavengerProcess/>
+      <MapContainer> 
+        <CustomMarker/>
+        <PoiMarkers/>
+      </MapContainer>
     </>
   );
 };
