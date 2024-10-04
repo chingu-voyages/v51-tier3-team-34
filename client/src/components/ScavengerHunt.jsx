0788d8984a591dac80e6be870d649ce504293df9
Julie Cheng <jucheng925@gmail.com>
2024-10-04 04:36:48 -0400

    scavenger hunt markers showing up on map

--- client/src/components/ScavengerHunt.jsx
+++ client/src/components/ScavengerHunt.jsx
@@ -32,12 +32,32 @@ const useGeolocation = (setUserLocation, accuracyThreshold = 50) => {
 
 const ScavengerHunt = () => {
   const [userLocation, setUserLocation] = useState(null);
+  const [huntLocations, setHuntLocations] = useState(null); // Scavenger hunt locations
   const accuracyThreshold = 50; // Threshold for location accuracy in meters
-  const {mapRef} = useContext(MapContext)
+
+  const apiUrl =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8080"
+    : import.meta.env.VITE_BACKEND_URL;
+
+//fetching scavenger hunt locations from backend
+  useEffect(() => {
+    fetch(`${apiUrl}/api/hunt-locations`)
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error(`HTTP error! Status: ${resp.status}`);
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setHuntLocations(data);
+      })
+      .catch((err) => console.error("Failed to fetch hunt locations", err));
+  }, []);
 
   useGeolocation(setUserLocation);
-
-
+  
   return (
     <>
       <div className="information">
