f9609ea6ef8eb3332249cd144edf446b62307639
Tayrika <rika13tay@gmail.com>
2024-10-04 15:55:00 +0900

    Add fetching logic for scavenger hunt locations

--- client/src/components/ScavengerHunt.jsx
+++ client/src/components/ScavengerHunt.jsx
@@ -25,13 +25,35 @@ const useGeolocation = (setUserLocation, accuracyThreshold = 50) => {
 
 const ScavengerHunt = () => {
   const [userLocation, setUserLocation] = useState(null);
+  const [huntLocations, setHuntLocations] = useState(null); // Scavenger hunt locations
   const accuracyThreshold = 50; // Threshold for location accuracy in meters
+
+  const apiUrl =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8080"
+    : import.meta.env.VITE_BACKEND_URL;
+
+//fetching scavenger hunt locations from backend
+  useEffect(() => {
+    fetch(`${apiUrl}/api/hunt-locations`)
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error(`HTTP error! Status: ${resp.status}`);
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setHuntLocations(data);
+      })
+      .catch((err) => console.error("Failed to fetch hunt locations", err));
+  }, []);
 
   useGeolocation(setUserLocation);
 
   return (
     <>
-      <MapContainer />
+      <MapContainer userLocation={userLocation} huntLocations={huntLocations} />
     </>
   );
 };
