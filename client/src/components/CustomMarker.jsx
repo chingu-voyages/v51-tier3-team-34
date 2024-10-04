fd98e326bcc390018c090f494e4fe43cfd4c94e2
Julie Cheng <jucheng925@gmail.com>
2024-10-04 00:58:37 -0400

    testing in process

--- client/src/components/CustomMarker.jsx
+++ client/src/components/CustomMarker.jsx
@@ -3,6 +3,7 @@ import { Marker } from '@react-google-maps/api';
 
 
 const CustomMarker = () => {
+  
     const position = { lat: 38.052327640, lng: -84.497 }; // Valid coordinates
 
     console.log('Rendering CustomMarker:', position); // Check if position is logged
