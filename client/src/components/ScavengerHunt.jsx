ffbb56db553e9cd88d8d51f8bfe1592e1025e0e2
Cody <c.jmiller17@yahoo.com>
2024-10-05 18:09:10 -0400

    Made a custom blue dot to show user location. There should be a pulsing effect and a cone showing user direction.

--- client/src/components/ScavengerHunt.jsx
+++ client/src/components/ScavengerHunt.jsx
@@ -3,14 +3,14 @@ import { MapContext } from "../context/MapContext"
 import MapContainer from "./MapContainer";
 import ScavengerProgress from "./ScavengerProgress"
 import ScavengerMarkers from "./ScavengerMarkers";
-import { Circle, DirectionsRenderer, Marker, useLoadScript } from "@react-google-maps/api";
+import { Circle, DirectionsRenderer, Marker, OverlayView } from "@react-google-maps/api";
 import ScavengerList from "./ScavengerList";
 import "../styles/scavenger.css"
 
 const center = {lat: 38.048172393597355, lng: -84.4964571176625}; // center of the entire scavenger area
 const center2 = { lat: 38.05224348731636, lng: -84.49533042381834}; // position of starting point
 
-const useGeolocation = (setUserLocation, accuracyThreshold = 50, startHunt, mapRef) => {
+const useGeolocation = (setUserLocation, accuracyThreshold = 50, startHunt, mapRef, setHeading) => {
   useEffect(() => {
     if (startHunt && "geolocation" in navigator) {
       const watchId = navigator.geolocation.watchPosition(
@@ -29,16 +29,34 @@ const useGeolocation = (setUserLocation, accuracyThreshold = 50, startHunt, mapR
               mapRef.current.panTo(newLocation);
             }
           } else {
-            console.warn("Location accuracy too low:", position.coords.accuracy);
+            console.warn(
+              "Location accuracy too low:",
+              position.coords.accuracy
+            );
           }
         },
         (error) => console.error("Error fetching geolocation", error),
         { enableHighAccuracy: true }
       );
 
-      return () => navigator.geolocation.clearWatch(watchId); // Cleanup on component unmount
+      // Capture heading data from the device's compass (if supported)
+      const handleOrientation = (event) => {
+        if (event.alpha !== null) {
+          const heading = 360 - event.alpha; // Get the compass heading (0-360)
+          console.log(event.alpha)
+          setHeading(heading); // Update heading state
+        }
+      };
+
+      window.addEventListener("deviceorientation", handleOrientation);
+
+      // Cleanup on component unmount
+      return () => {
+        navigator.geolocation.clearWatch(watchId);
+        window.removeEventListener("deviceorientation", handleOrientation);
+      };
     }
-  }, [setUserLocation, accuracyThreshold , startHunt, mapRef]);
+  }, [setUserLocation, accuracyThreshold , startHunt, mapRef, setHeading]);
 };
 
 const ScavengerHunt = () => {
@@ -46,9 +64,9 @@ const ScavengerHunt = () => {
   const [startHunt, setStartHunt] = useState(false)
   const [userLocation, setUserLocation] = useState(null); // Current user location
   const [huntLocations, setHuntLocations] = useState(null); // Scavenger hunt locations
-  const [userProgress, setUserProgress] = useState(0) 
-      // keeping track of how many location user has visited, will increase by 1 after a location is found (max: 10 - completed hunt)
+  const [userProgress, setUserProgress] = useState(0) // keeping track of how many location user has visited, will increase by 1 after a location is found (max: 10 - completed hunt)
   const [userPoints, setUserPoints] = useState(0); // keeping track of user points
+  const [heading, setHeading] = useState(0) // Users compass heading
 
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [routeSegments, setRouteSegments] = useState([])
@@ -76,7 +94,7 @@ const ScavengerHunt = () => {
   }, []);
 
   // Use geolocation when the hunt starts
-  useGeolocation(setUserLocation, 50, startHunt);
+  useGeolocation(setUserLocation, 50, startHunt, mapRef, setHeading);
 
   const handleClick = () =>{
     setStartHunt(true)
@@ -133,6 +151,7 @@ const ScavengerHunt = () => {
       setRouteSegments(segments);
     };
   }
+  
   const checkLocation = () => {
     if (!userLocation || !huntLocations || userProgress >= huntLocations.length) return;
     
@@ -160,78 +179,127 @@ const ScavengerHunt = () => {
 
   return (
     <>
-      {!startHunt ? 
+      {!startHunt ? (
         <div className="information">
           <h2>Scavenger Hunt</h2>
-          <p>Ready to test your knowledge of Lexington, KY? Earn points playing our scavenger hunt.</p>
-          <p>If you are ready, head to downtown Lexington to the marker on the map. Once you are there, click on the button to start. Note will turn on GPS monitoring.</p>
-          <button onClick={handleClick}>I am here!</button> {/* Once clicked, can turn on GPS*/ }
-        </div> 
-        :
+          <p>
+            Ready to test your knowledge of Lexington, KY? Earn points playing
+            our scavenger hunt.
+          </p>
+          <p>
+            If you are ready, head to downtown Lexington to the marker on the
+            map. Once you are there, click on the button to start. Note will
+            turn on GPS monitoring.
+          </p>
+          <button onClick={handleClick}>I am here!</button>{" "}
+          {/* Once clicked, can turn on GPS*/}
+        </div>
+      ) : (
         <div className="hunt-interface">
-          <ScavengerProgress huntLocations={huntLocations} userProgress={userProgress}/>
+          <ScavengerProgress
+            huntLocations={huntLocations}
+            userProgress={userProgress}
+          />
           <p>Points: {userPoints}</p>
 
           {/*BUTTON can be deleted or use if gps is not working well?*/}
-          <button onClick={()=>{
-            if (userProgress < 10){
-              setUserProgress((prev)=>prev +1)
-            } 
-          }}>
+          <button
+            onClick={() => {
+              if (userProgress < 10) {
+                setUserProgress((prev) => prev + 1);
+              }
+            }}
+          >
             TEST button for user progression
           </button>
         </div>
-      }
-      {huntLocations &&
+      )}
+      {huntLocations && (
         <div className="display-section">
-        <ScavengerList huntLocations={huntLocations} userProgress={userProgress}/>
-        <MapContainer center={userLocation ? userLocation : center2} zoom={15}> 
-          {!startHunt ? 
-          <Marker position={center2} /> :
-          <>
-          <Circle 
-            center ={center} 
-            radius={530}
-            options={{
-              strokeWeight: 0.5,
-              fillOpacity: 0.08
-            }} 
+          <ScavengerList
+            huntLocations={huntLocations}
+            userProgress={userProgress}
           />
-          {userLocation && (
-            // Display user's location with a blue dot
-            <Marker
-              position={userLocation}
-              icon={{
-                url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-                scaledSize: new window.google.maps.Size(40, 40), 
-              }}
-            />
-          )}
-          <ScavengerMarkers huntLocations={huntLocations} userProgress={userProgress}/>
-          {routeSegments.length > 0 && userProgress > 1 && (
-          <DirectionsRenderer
-            directions={{ 
-              ...directionsResponse,
-              routes: [{
-                legs: routeSegments.slice(0, userProgress - 1)  // Render up to current progress
-              }]
-            }}
-            options={{
-              suppressMarkers: true,
-              polylineOptions: {
-                strokeColor: "#69c261",          // Customize the route color (red)
-                strokeOpacity: 0.7,              // Customize the opacity of the route
-                strokeWeight: 5,                 // Customize the width of the route
-              },
-              suppressInfoWindows: true          // Disable default info windows
-            }}
-            />
-          )}
-          </>
-          }
-        </MapContainer>
+          <MapContainer
+            center={userLocation ? userLocation : center2}
+            zoom={15}
+          >
+            {!startHunt ? (
+              <Marker position={center2} />
+            ) : (
+              <>
+                <Circle
+                  center={center}
+                  radius={530}
+                  options={{
+                    strokeWeight: 0.5,
+                    fillOpacity: 0.08,
+                  }}
+                />
+                {userLocation && (
+                  <>
+                    {/* Display user's location with a blue dot */}
+                    <Marker
+                      position={userLocation}
+                      icon={{
+                        path: google.maps.SymbolPath.CIRCLE, // A directional triangle
+                        fillColor: "#0096FF",
+                        fillOpacity: 1,
+                        scale: 7,
+                        rotation: heading, // Rotate the triangle based on user heading
+                        strokeWeight: 1.4,
+                        strokeColor: "#007AFF",
+                      }}
+                    />
+
+                    {/* Use OverlayView to render custom elements at the user's location */}
+                    <OverlayView
+                      position={userLocation}
+                      mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+                    >
+                      <div
+                        style={{ position: "relative", width: 0, height: 0 }}
+                      >
+                        <div className="pulse-circle" />
+                        <div
+                          className="directional-cone"
+                          style={{ transform: `rotate(${heading - 26}deg)` }}
+                        />
+                      </div>
+                    </OverlayView>
+                  </>
+                )}
+                <ScavengerMarkers
+                  huntLocations={huntLocations}
+                  userProgress={userProgress}
+                />
+
+                {routeSegments.length > 0 && userProgress > 1 && (
+                  <DirectionsRenderer
+                    directions={{
+                      ...directionsResponse,
+                      routes: [
+                        {
+                          legs: routeSegments.slice(0, userProgress - 1), // Render up to current progress
+                        },
+                      ],
+                    }}
+                    options={{
+                      suppressMarkers: true,
+                      polylineOptions: {
+                        strokeColor: "#69c261", // Customize the route color (red)
+                        strokeOpacity: 0.7, // Customize the opacity of the route
+                        strokeWeight: 5, // Customize the width of the route
+                      },
+                      suppressInfoWindows: true, // Disable default info windows
+                    }}
+                  />
+                )}
+              </>
+            )}
+          </MapContainer>
         </div>
-      }
+      )}
     </>
   );
 };
