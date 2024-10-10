5f64b72f9f8430e7949724ae821487ff384f1631
Tayrika <rika13tay@gmail.com>
2024-10-11 00:00:40 +0900

    Delete dummy data

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -9,20 +9,6 @@ const Leaderboard = () => {
   const [users, setUsers] = useState([]);
   const [showFullList, setShowFullList] = useState(false);
 
-  // // Replace this with actual data
-  // const users = [
-  //   { id: 1, name: "User 1", img: "user1.png", score: 500, },
-  //   { id: 2, name: "User 2", img: "user2.png", score: 400, },
-  //   { id: 3, name: "User 3", img: "user3.png", score: 300, },
-  //   { id: 4, name: "User 4", img: "user4.png", score: 200, },
-  //   { id: 5, name: "User 5", img: "user5.png", score: 100, },
-  //   { id: 6, name: "User 6", img: "user6.png", score: 50, },
-  //   { id: 7, name: "User 7", img: "user7.png", score: 40, },
-  //   { id: 8, name: "User 8", img: "user8.png", score: 30, },
-  //   { id: 9, name: "User 9", img: "user9.png", score: 20, },
-  //   { id: 10, name: "User 10", img: "user10.png", score: 10, },
-  // ];
-
   // Fetch users from backend
   useEffect(() => {
     const fetchUsers = async () => {
